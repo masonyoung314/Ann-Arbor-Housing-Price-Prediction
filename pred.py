d13b6727e0abbca40104188e3b96b6971a2dcd24
Mason Young <masonyou@umich.edu>
2026-05-10 18:52:16 +0100

    XGBRegression, next try logistic regression

--- pred.py
+++ pred.py
@@ -5,8 +5,11 @@ import numpy as np
 import numpy.typing as npt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import RepeatedKFold
+from sklearn.model_selection import cross_val_score
+import random
 
-
+# Convert year-month-day to float of seconds since 1970
 def date_to_float(df: pd.DataFrame) -> pd.DataFrame:
     df["sale_date"] = pd.to_datetime(df["sale_date"])
     df["sale_date"] = df["sale_date"].astype("int64") / 1e9
@@ -60,13 +63,73 @@ def get_data(X: npt.NDArray, y: npt.NDArray) -> tuple[npt.NDArray, npt.NDArray, 
 
     y = cleanup_weird_vals(y)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X,
-        y,
-        test_size=0.2
-    )
+    # X_train, X_test, y_train, y_test = train_test_split(
+    #     X,
+    #     y,
+    #     test_size=0.2
+    # )
 
-    return X_train, X_test, y_train, y_test 
+    # return X_train, X_test, y_train, y_test 
+
+    return X, y
+
+def print_score_info(scores, rmse_scores):
+    print(f"Scores raw values: {scores}")
+    print(f"Mean of scores: {scores.mean()}")
+    print(f"Standard deviation of scores: {scores.std()}")
+
+    print(f"Mean of RMSE scores: {rmse_scores.mean()}")
+    print(f"Standard devation of RMSE scores: {rmse_scores.std()}")
+
+def find_best_model(X, y):
+    # Randomized grid search to find best XGBRegressor hyperparameters
+    # Test 20 random combinations, take the best one
+    max_depths = [3, 5, 7]
+    n_estimators = 200
+    learning_rates = [0.03, 0.05, 0.1]
+    subsamples = [0.7, 0.8, 1]
+    colsample_bytrees = [0.7, 0.8, 1]
+
+    best_combination = None
+    best_mean = None
+    
+    for i in range(0, 21):
+        depth = random.choice(max_depths)
+        learning_rate = random.choice(learning_rates)
+        subsample = random.choice(subsamples)
+        colsample = random.choice(colsample_bytrees)
+
+        model = xgb.XGBRegressor(n_estimators=n_estimators, max_depth=depth, eta=learning_rate, subsample=subsample, colsample_bytree=colsample)
+        cv = RepeatedKFold(n_splits=5, n_repeats=3, random_state=42)
+        scores = cross_val_score(model, X, y, cv=cv, scoring='r2')
+
+        if i == 0 or (scores.mean() > best_mean):
+            best_combination = depth, learning_rate, subsample, colsample
+            best_mean = scores.mean()
+
+    # for max_depth in max_depths:
+    #     for learning_rate in learning_rates:
+    #         for subsample in subsamples:
+    #             for colsample in colsample_bytrees:
+    #                 model = xgb.XGBRegressor(n_estimators=n_estimators, max_depth=max_depth, eta=learning_rate, subsample=subsample, colsample_bytree=colsample)
+    #                 cv = RepeatedKFold(n_splits=5, n_repeats=3, random_state=42)
+    #                 scores = cross_val_score(model, X, y, cv=cv, scoring='r2')
+
+    #                 if best_combination == None or (scores.mean() < best_mean):
+    #                     best_combination = max_depth, learning_rate, subsample, colsample
+    #                     best_mean = scores.mean()
+
+    print("Best combination of hyperparameters: ")
+    print(f"Max_Depth: {best_combination[0]}")
+    print(f"Eta: {best_combination[1]}")
+    print(f"Subsample: {best_combination[2]}")
+    print(f"Colsample_bytree: {best_combination[3]}")
+    print(f"Best mean R^2: {best_mean}")
+
+    return best_combination[0], best_combination[1], best_combination[2], best_combination[3]
+
+
+
 
 def main():
 
@@ -80,21 +143,34 @@ def main():
     housing_pd = date_to_float(housing_pd)
     X = get_feature_vectors(housing_pd).to_numpy()
     y = housing_pd["sale_price"].to_numpy()
-    print(y)
 
-    X_train, X_test, y_train, y_test = get_data(X, y)
+    # X_train, X_test, y_train, y_test = get_data(X, y)
+    X, y = get_data(X, y)
 
-    # print(f"# houses with missing price: {np.sum(pd.isnull(housing[:, 0]))}")
-    # print(f"# houses with price < 5000: {np.size(np.where(housing[:, 0] < 5000))}")
-    # print(f"# houses with price < 20000: {np.size(np.where(housing[:, 0] < 20000))}")
-    # print(f"# houses with price < 50000: {np.size(np.where(housing[:, 0] < 50000))}")
+    # mdl = xgb.XGBRegressor(n_estimators=200, max_depth=7, eta=0.1, subsample=0.7, colsample_bytree=0.8)
 
-    # with np.printoptions(threshold=np.inf):
-    #     print(housing[housing[:, 0] < 50000])
+    # cv = RepeatedKFold(n_splits=5, n_repeats=3, random_state=1)
 
+    # scores = cross_val_score(mdl, X, y, cv=cv, scoring='r2')
+    # rmse_scores = -cross_val_score(
+    #     mdl,
+    #     X,
+    #     y,
+    #     scoring='neg_root_mean_squared_error',
+    #     cv=cv
+    # )
 
-    # print(housing_pd.isna().sum())
+    # print_score_info(scores, rmse_scores)
 
+    depth, learning_rate, subsample, colsample = find_best_model(X, y)
+
+    best_model = xgb.XGBRegressor(n_estimators=1000, max_depth=depth, eta=learning_rate, subsample=subsample, colsample_bytree=colsample)
+
+    cv = RepeatedKFold(n_splits=5, n_repeats=3, random_state=42)
+
+    scores = cross_val_score(best_model, X, y, cv=cv, scoring='r2')
+    rmse_scores = -cross_val_score(best_model, X, y, cv=cv, scoring='neg_root_mean_squared_error')
+    print_score_info(scores, rmse_scores)
 
 
 if __name__ == "__main__":
